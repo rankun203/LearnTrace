cfc2d47da77fe6c5bcba283c945ad699e8869982
Lov Why <rankun203@gmail.com>
2013-01-04 16:29:12 +0800

    Spring配置DataSource

--- SpringAOPStart/src/com/mindfine/spring/dao/impl/IStudentImpl.java
+++ SpringAOPStart/src/com/mindfine/spring/dao/impl/IStudentImpl.java
@@ -4,6 +4,12 @@ import com.mindfine.spring.dao.IStudentDAO;
 import com.mindfine.spring.model.Student;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: rankun203
@@ -12,8 +18,24 @@ import org.springframework.stereotype.Component;
  */
 @Component("stuImpl")
 public class IStudentImpl implements IStudentDAO {
+    DataSource datasource;
     @Override
     public void save(Student student) {
         System.out.println("IStudentImpl report: " + student + " saved.");
+        ResultSet rs = null;
+        try {
+            Connection dbc = datasource.getConnection();
+            rs = dbc.createStatement().executeQuery("select * from Persons");
+            while(rs.next()){
+                System.out.println("IStudentImpl report: data from db- " + rs.getString("FirstName"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Resource(name = "dataSource")
+    public void setDbc(DataSource ds) throws SQLException {
+        this.datasource = ds;
     }
 }
